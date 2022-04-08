3a7d4031e9e46b89deb286d3518389d8ec74a22f
Objective_Charm <69391643+ObjectiveCharm@users.noreply.github.com>
2022-04-08 16:23:22 +0800

    Edit Package.swift trying to make it compatible with current swiftPM  (#32)
    * Update Package.swift
    
    It seems that swiftPM has changed and now requires to specify swift language version.
    
    * Update Package.swift
    
    * Update Package.swift
    
    make the Package.swift compatible with swiftPM

--- Package.swift
+++ Package.swift
@@ -1,7 +1,15 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "SwiftCron",
-    targets: [],
-    dependencies:[]
+    products: [
+        .library(name: "SwiftCron", targets: ["SwiftCron"]),
+    ],
+    dependencies:[],
+        targets: [
+        .target(name: "SwiftCron", dependencies: [], path: "Sources/")
+    ],
+    swiftLanguageVersions: [.v5]
 )
